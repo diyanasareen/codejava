ae839fe1ccd6fdad82466ab1e14599608e9bb83e
diyanasareen <79687082+diyanasareen@users.noreply.github.com>
2021-03-09 20:19:15 +0530

    Add files via upload

---
+++ checkbox1.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class checkbox1 extends Frame
+{
+ checkbox1()
+  {
+    addWindowListener(new WindowAdapter()
+	{
+	 public void windowClosing(WindowEvent e)
+	  {System.exit(0);}
+	});
+
+Checkbox J,P,C;
+
+Frame frame=new Frame("Checkbox1");
+c1=new Checkbox("Java",false);
+c2=new Checkbox("Python",false);
+c3=new Checkbox("C++",false);
+
+c1.setBounds  (20,50,160,30);
+add(c1);
+c2.setBounds (20,90,160,30);
+add(c2);
+c3.setBounds (20,130,160,30);
+add(c3);
+
+setSize(500,500);
+setVisible(true);
+
+}
+ public static void main (String args[])
+   {
+  checkbox1 frame=new checkbox1();
+   }
+}
+
+
+
+
+
+
